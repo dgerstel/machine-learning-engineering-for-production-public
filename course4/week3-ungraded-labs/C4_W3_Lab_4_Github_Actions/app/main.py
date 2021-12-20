12df9586ec79feb70a69ad00de84dc37101c7e69
Dawid Gerstel <dawidgerstel@gmail.com>
2021-12-20 11:26:03 +0100

    Test our CI/CD pipeline

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
 
-
+# Here we start our FastAPI App
 app = FastAPI(title="Predicting Wine Class with batching")
 
 # Open classifier in global scope
